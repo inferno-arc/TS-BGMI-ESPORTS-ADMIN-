498921caf583f5b70bc2bcaa95d14b09f4da09b6
inferno-arc <ganeshrathod3440@gmail.com>
2026-08-19 09:28:57 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,82 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+  getAuth,
+  sendSignInLinkToEmail,
+  isSignInWithEmailLink,
+  signInWithEmailLink,
+  signOut,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+
+const firebaseConfig = {
+
+  apiKey:
+    "AIzaSyD4Q9K8yuV91KfZ0_AZyCsXKwXZn2X1BKI",
+
+  authDomain:
+    "tsbgmi-esports.firebaseapp.com",
+
+  projectId:
+    "tsbgmi-esports",
+
+  storageBucket:
+    "tsbgmi-esports.firebasestorage.app",
+
+  messagingSenderId:
+    "398186419907",
+
+  appId:
+    "1:398186419907:web:5a84376390a33a4c167476",
+
+  measurementId:
+    "G-NVVTEMTE02"
+
+};
+
+
+const app =
+  initializeApp(firebaseConfig);
+
+
+const auth =
+  getAuth(app);
+
+
+const db =
+  getFirestore(app);
+
+
+export {
+
+  app,
+  auth,
+  db,
+
+  sendSignInLinkToEmail,
+  isSignInWithEmailLink,
+  signInWithEmailLink,
+  signOut,
+  onAuthStateChanged,
+
+  collection,
+  addDoc,
+  doc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  serverTimestamp
+
+};
